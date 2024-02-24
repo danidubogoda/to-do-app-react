09cac418a2582a8a62e2d6fc4acb54aa6791ea16
Danidu Bogoda <danidubogoda95@gmail.com>
2024-02-24 09:51:40 +0530

    Update packages

---
+++ front-end/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {Header} from "./header/Header.tsx";
+
+
+function App() {
+
+
+  return (
+        <>
+          <Header/>
+        </>
+  )
+}
+
+export default App
---
+++ front-end/src/header/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+export function Header(){
+
+    return(
+        <header className='p-2 border-bottom'>
+            <h1 className='text-primary'>To Do App</h1>
+
+        </header>
+    )
+
+}
